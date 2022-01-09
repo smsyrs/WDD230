dfcbfad2869ae7955e4d7a200b1f72ded49bea3b
smsyrs <ssyrs1@byui.edu>
2022-01-08 22:54:56 -0800

    less jank copyright

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const date = new Date();
 let currentYear = date.getFullYear();
 
-const footer = document.querySelector("footer");
-footer.textContent = `\u00a9 ${currentYear} Samantha Jenkins Washington`;
+//const footer = document.querySelector("footer");
+//footer.textContent = `\u00a9 ${currentYear} Samantha Jenkins Washington`;
 
+document.getElementById("copyright-year").innerHTML = `\u00a9 ${currentYear}`;
